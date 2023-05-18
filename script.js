a29877c0b641d5bd5dad7804cc700b6e7885b092
EmadNova <im.nova@yahoo.com>
2023-05-18 21:40:40 +0330

    fix: fixing some effects before swiper

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const burgerIcon = document.querySelector(".burger-menu-icon");
 const burgerScreen = document.querySelector(".burger-menu-screen");
+const rotateOne = document.querySelector(".burger-menu-icon__line--one")
+const rotateTwo = document.querySelector(".burger-menu-icon__line--two")
+const rotateThree = document.querySelector(".burger-menu-icon__line--three")
+const arrow = document.querySelector(".main__arrow-down");
+
 
 burgerIcon.addEventListener("click", () => {
     burgerScreen.classList.toggle("active-burger-menu-screen");
+    rotateOne.classList.toggle('burger-menu-icon--rotate--one');
+    rotateTwo.classList.toggle('burger-menu-icon--rotate--two');
+    rotateThree.classList.toggle('burger-menu-icon--rotate--three');
 })
 
+arrow.addEventListener("click", () => {
+    window.scroll({
+        top: 760,
+        behavior: "smooth",
+    })
+})
+
+
+//
+// window.document.addEventListener('scroll', () => {
+//     console.log(window.scrollY);
+// })
