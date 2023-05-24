005904febcabf481f77a801737aee154a0a2683c
EmadNova <im.nova@yahoo.com>
2023-05-24 16:13:46 +0330

    feat & fix: issues are fixed and features all added to the  project.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const rotateTwo = document.querySelector(".burger-menu-icon__line--two")
 const rotateThree = document.querySelector(".burger-menu-icon__line--three")
 const arrow = document.querySelector(".main__arrow-down");
 const arrowUp = document.querySelector(".arrowUp");
+const li = document.querySelectorAll(".burger-menu-screen__list__each")
 
 
 arrowUp.addEventListener("click", () => {
@@ -21,17 +22,47 @@ burgerIcon.addEventListener("click", () => {
     rotateThree.classList.toggle('burger-menu-icon--rotate--three');
 })
 
-arrow.addEventListener("click", () => {
-    window.scroll({
-        top: 950,
-        behavior: "smooth",
+document.onclick = function (e) {
+    if (!burgerScreen.contains(e.target) && !burgerIcon.contains(e.target)) {
+        burgerScreen.classList.remove("active-burger-menu-screen");
+        rotateOne.classList.remove('burger-menu-icon--rotate--one');
+        rotateTwo.classList.remove('burger-menu-icon--rotate--two');
+        rotateThree.classList.remove('burger-menu-icon--rotate--three');
+    }
+}
+
+li.forEach((value) => {
+    value.addEventListener("click", () => {
+        burgerScreen.classList.remove("active-burger-menu-screen");
+        rotateOne.classList.remove('burger-menu-icon--rotate--one');
+        rotateTwo.classList.remove('burger-menu-icon--rotate--two');
+        rotateThree.classList.remove('burger-menu-icon--rotate--three');
     })
 })
 
+if (window.innerWidth <= 320) {
+    arrow.addEventListener("click", () => {
+        window.scrollTo(0, 820)
+    })
+} else if (window.innerWidth <= 480 && window.innerWidth <= 786) {
+    arrow.addEventListener("click", () => {
+        window.scrollTo(0, 740)
+    })
+} else if (window.innerWidth >= 1201) {
+    arrow.addEventListener("click", () => {
+        window.scrollTo(0, 750)
+    })
+}
 
+window.addEventListener("scroll", () => {
 
+    if (window.scrollY >= 900) {
+        arrowUp.style.display = "block";
+    } else {
+        arrowUp.style.display = "none";
+    }
+})
 
-//
 // window.document.addEventListener('scroll', () => {
 //     console.log(window.scrollY);
 // })
